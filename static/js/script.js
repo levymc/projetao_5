dc7f0748e345821973904aa6f61c5875c25ccc65
levymc <levymcruz@gmail.com>
2023-04-10 22:52:55 -0300

    arrumando o JS de acesso

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,27 @@
-// const url = "https://mock-api.driven.com.br/api/v6/uol/participants"
+const url = "https://mock-api.driven.com.br/api/v6/uol/participants"
+// const url = 'https://mock-api.driven.com.br/api/v6/uol/status';
+// const url = 'http://mock-api.driven.com.br/api/v6/uol/messages'
+
 const form = document.querySelector('form');
 
-const nomeDigitado = document.querySelector('#name').value;
-const dados = {
-  name: nomeDigitado,
-};
-const dadosJSON = JSON.stringify(dados);
+document.querySelector('#submit-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    const nomeDigitado = document.querySelector('#name').value;
+    const dados = {
+        name: nomeDigitado,
+    };
+    console.log(dados);
+    fetch(url, {
+        method: 'POST',
+        body: dados
+    })
+    .then(response => response.json())    
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+});
 
-const url = 'https://mock-api.driven.com.br/api/v6/uol/status';
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: dadosJSON
-  })
-  .then(response => response.json())    
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+
   
 
   
