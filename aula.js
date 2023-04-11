bdeaae0ac17e3cdb8d01d36fe5f4838033b0233d
levymc <levymcruz@gmail.com>
2023-04-10 21:20:35 -0300

    aula.js added

---
+++ aula.js
@@ -0,0 +1,6 @@
+ingredientes = [
+    {receita: "Bolo de Chocolate", ingredientes: "farinha", preparo: "bate tudo"},
+    {receita: "Pudim de Leite Condensado", ingredientes: "1 lata de leite condensado, 1 lata (leite condensado) de leite e 3 Ovos", preparo:"Preparo do Pudim"},
+    {receita: "Doce de Leite", ingredientes: "2l de leite, 200g de açucar e 1 colher de chá de bicarbonato", preparo:"bla bla bla"},
+]
+console.log(ingredientes)
