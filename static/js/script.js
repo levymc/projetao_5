a20a5050cd9bd2480ac4e66f8ed9c47be611b552
levymc <levymcruz@gmail.com>
2023-04-10 23:10:24 -0300

    enviando msg do input pra outra pagina

--- static/js/script.js
+++ static/js/script.js
@@ -3,22 +3,58 @@ const url = "https://mock-api.driven.com.br/api/v6/uol/participants"
 // const url = 'http://mock-api.driven.com.br/api/v6/uol/messages'
 
 const form = document.querySelector('form');
+let newUser = '';
 
-document.querySelector('#submit-button').addEventListener('click', function(event) {
+function novoAcesso(newUser){
+    const lista = document.querySelector('.msg-list');
+    console.log(newUser);
+
+    const agora = new Date();
+    const hora = agora.getHours();
+    const minuto = agora.getMinutes();
+    const segundo = agora.getSeconds();
+
+    const html = `
+    <li class="msg-access">
+        <div class="msg-hora">(${hora}:${minuto}:${segundo})</div>
+        <div class="msg-text">${newUser.name} entra na sala...</div>
+    </li>
+    `;
+
+    lista.insertAdjacentHTML('beforeend', html)
+
+};
+
+document.querySelector('#entrar').addEventListener('click', function(event) {
     event.preventDefault();
-    const nomeDigitado = document.querySelector('#name').value;
-    const dados = {
-        name: nomeDigitado,
-    };
-    console.log(dados);
-    fetch(url, {
-        method: 'POST',
-        body: dados
-    })
-    .then(response => response.json())    
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    newUser ={
+        name:document.querySelector('#name').value,
+    } 
+    novoAcesso(newUser);
+    // console.log(newUser);
+    window.location.replace('index.html');
 });
+
+
+
+
+
+
+// document.querySelector('NOMEDOBOTAOAQUI').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     const nomeDigitado = document.querySelector('#name').value;
+//     const dados = {
+//         name: nomeDigitado,
+//     };
+//     console.log(dados);
+//     fetch(url, {
+//         method: 'POST',
+//         body: dados
+//     })
+//     .then(response => response.json())    
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+// });
 
 
 
