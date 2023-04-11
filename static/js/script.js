c77bd7aa48d7b8783af5f57a3cef850596668fa2
levymc <levymcruz@gmail.com>
2023-04-11 19:47:18 -0300

    criando os objetos

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,8 @@ const url = "https://mock-api.driven.com.br/api/v6/uol/participants"
 
 // const form = document.querySelector('form');
 
+axios.defaults.headers.common['Authorization'] = 'knX7klq0NWrHzRzsc8JXodY0';
+
 let newUser;
 const agora = new Date();
 const hora = agora.getHours();
@@ -21,17 +23,38 @@ window.addEventListener('load', function() {
 function newPost(type){
     let classe;
     let frase;
+    let quem;
+    let dados = {};
     const msg = document.querySelector(".msg-input input").value;
+    
     if (type == 1){
         classe = 'msg-access';
         frase = `<div class="msg-text">${newUser} entra na sala...</div>`;
+        dados = {
+            name: newUser,
+          }
     } else if (type == 2){
         classe = 'msg-private';
         frase = `<div class="msg-text">${newUser} reservadamente para Maria: ${msg}</div>`;
+        dados = {
+            from: newUser,
+            to: "Nome do Destinatario",
+            text: frase,
+            type: 'private-message',
+            time: `${hora}:${minuto}:${segundo}`
+        }
     } else{
         classe = 'msg-message';
         frase = `<div class="msg-text">${newUser} para Todos: ${msg}</div>`;
+        dados = {
+            from: newUser,
+            to: "Nome do Destinatario",
+            text: frase,
+            type: 'message',
+            time: `${hora}:${minuto}:${segundo}`
+        }
     }
+    
     const lista = document.querySelector('.msg-list');
     const html = `
     <li class="${classe}">
